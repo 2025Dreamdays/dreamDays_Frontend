15d768ed287ddbbc7637411e9216c51dbc0eb489
sieun2im <kse040531@naver.com>
2025-02-19 03:48:43 +0900

    again

---
+++ main.js
@@ -0,0 +1,47 @@
+const registerInfo = document.querySelector('.registerInfo');
+const checkInfo = document.querySelector('.checkInfo');
+const orangeDisplay = document.querySelector('.orangeDisplay');
+const blackDisplay = document.querySelector('.blackDisplay');
+const orange = document.querySelector('.orange');
+const black = document.querySelector('.black');
+const likelion = document.querySelector('.likelion');
+const body = document.querySelector('body');
+
+
+//화면 넘김 모션
+const clickCheck = () => {
+    orangeDisplay.classList.add('none');
+    blackDisplay.classList.add('none');
+    orange.classList.add('maxWidth');
+    black.classList.add('minWidth');
+    likelion.classList.add('none')
+}
+const clickRegister = () => {
+    orangeDisplay.classList.add('none');
+    blackDisplay.classList.add('none');
+    orange.classList.add('minWidth');
+    black.classList.add('maxWidth');
+    likelion.classList.add('none')
+}
+checkInfo.addEventListener('click', clickCheck);
+registerInfo.addEventListener('click', clickRegister);
+
+
+//사전등록 확인 버튼 비활성화 코드
+let now = new Date();
+
+let dreamD = new Date("2025-02-19");
+
+console.log(now > dreamD);
+if (now > dreamD) {
+    checkInfo.disabled = true;
+}
+
+
+//입력한 정보 확인 페이지 이동
+const checkDisplayLink = () => {
+    setTimeout(() => {
+        window.location.href = "file:///C:/Users/sieun/Desktop/dd/dreamDays_Frontend/checkInfo.html"
+    }, 1100)
+}
+checkInfo.addEventListener('click', checkDisplayLink);
---
+++ viewInfo.js
@@ -0,0 +1,5 @@
+const friendsButton = document.getElementById('friendsButton');
+const checkDisplayLink = () => {
+    window.location.href = "file:///C:/Users/sieun/Desktop/dd/dreamDays_Frontend/loading.html"
+}
+friendsButton.addEventListener('click', checkDisplayLink);
