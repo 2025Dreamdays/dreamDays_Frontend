ced7eb450d62c57e42d7350b02de3e42cda9116d
sieun2im <kse040531@naver.com>
2025-02-21 20:49:22 +0900

    i am happy

--- viewInfo.js
+++ viewInfo.js
@@ -3,3 +3,9 @@ const checkDisplayLink = () => {
     window.location.href = "file:///C:/Users/sieun/Desktop/dd/dreamDays_Frontend/loading.html"
 }
 friendsButton.addEventListener('click', checkDisplayLink);
+
+axios.get("http://swapi.dev/api/people/1")
+    .then(res => {
+        console.log("Response:", res)
+    })
+
