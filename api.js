28a428b068c7dc7559477dd36573db70bbced3b9
biyuns <cart0089@naver.com>
2025-02-22 04:16:49 +0900

    dkfdkf

--- api.js
+++ api.js
@@ -1,17 +1,21 @@
 document.getElementById("suss").addEventListener("click", async function () {
     const data = {
         name: document.getElementById("ip_name").value,
+        instagramId: document.querySelector(".ip_id").value,
         age: document.getElementById("ip_age").value,
         gender: document.getElementById("men_ck").checked ? "남성" : document.getElementById("women_ck").checked ? "여성" : "",
-        major: document.getElementById("dp_major").value,
+        department: document.getElementById("dp_major").value,
         mbti: document.getElementById("dp_mbti").value,
-        insta_id: document.querySelector(".ip_id").value,
-        one_liner: document.querySelector(".ip_liner").value,
-        it_gender: [...document.querySelectorAll(".ip_it_gender:checked")].map(gender => gender.nextElementSibling.textContent)
-    };
+        selectGender: [...document.querySelectorAll(".ip_it_gender:checked")].map(gender => gender.nextElementSibling.textContent),
+        bio: document.querySelector(".ip_liner").value,
+            };
 
     try {
-        const response = await axios.post("http://3.39.44.144:8080/api/users/create-info", data);
+        axios.defaults.baseURL = 'http://3.39.44.144:8080'; // 기본 URL 설정
+
+const response = await axios.post("/api/users/create-info", data);
+
+
 
         if (response.status === 200) {
             alert("백엔드 연동 성공! 서버에서 응답 받음 ✅");
