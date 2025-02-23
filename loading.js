21efb4d5462469c6bae34f4fd6c3c0eb4c63d5e0
biyuns <cart0089@naver.com>
2025-02-24 05:07:17 +0900

    살려주세요제발

--- loading.js
+++ loading.js
@@ -33,40 +33,42 @@
 
 // }, 2000)
 
-async function drawUser() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const studentNumber = urlParams.get('studentNumber');
-    const name = urlParams.get('name');
-    const requestData = {
-        name: `${name}`,
-        studentNumber: `${studentNumber}`
-    };
+// async function drawUser() {
+//     const urlParams = new URLSearchParams(window.location.search);
+//     const studentNumber = urlParams.get('studentNumber');
+//     const name = urlParams.get('name');
+//     const requestData = {
+//         name: `${name}`,
+//         studentNumber: `${studentNumber}`
+//     };
 
-    try {
-        const response = await fetch("https://likelion.hellofriend.cc/api/draw", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(requestData)
-        });
+//     try {
+//         const response = await fetch("https://likelion.hellofriend.cc/api/draw", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify(requestData)
+//         });
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! Status: ${response.status}`);
+//         }
 
-        const data = await response.json();
-        console.log("뽑힌 사용자 정보:", data);
+//         const data = await response.json();
+//         console.log("뽑힌 사용자 정보:", data);
 
-        // 뽑힌 사용자 정보 추출
-        const drawnUser = data;  // 백엔드에서 보내주는 사용자 정보 그대로 받기
+//         // 뽑힌 사용자 정보 추출
+//         const drawnUser = data;  // 백엔드에서 보내주는 사용자 정보 그대로 받기
 
-        // 쿼리 파라미터로 데이터를 넘기기
-        window.location.href = `result_page.html?name=${drawnUser.name}&age=${drawnUser.age}&instagramId=${drawnUser.instagramId}&department=${drawnUser.department}&gender=${drawnUser.gender}&mbti=${drawnUser.mbti}&bio=${drawnUser.bio}`;        
-    } catch (error) {
-        console.error("뽑기 실패:", error);
-    }
-}
+//         // 쿼리 파라미터로 데이터를 넘기기
+//         window.location.href = `result_page.html?name=${drawnUser.name}&age=${drawnUser.age}&instagramId=${drawnUser.instagramId}&department=${drawnUser.department}&gender=${drawnUser.gender}&mbti=${drawnUser.mbti}&bio=${drawnUser.bio}`;        
+//     } catch (error) {
+//         console.error("뽑기 실패:", error);
+//     }
+// }
+
+window.location.href = `result_page.html`;
 
 
 
