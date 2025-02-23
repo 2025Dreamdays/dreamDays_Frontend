cbd15133de0a36a1eb83c30c284d575cacc6c528
biyuns <cart0089@naver.com>
2025-02-24 01:18:55 +0900

    dfkjfjklfdlkjdf

--- api_view.js
+++ api_view.js
@@ -4,8 +4,8 @@ fetch('https://likelion.hellofriend.cc/api/users/check-info')
         console.log('서버 응답:', data);
     
 
-        document.getElementById('name').textContent = `이름: ${data.name}`;
-        document.getElementById('studentNumber').textContent = `학번: $${data.student_id}`;
+        document.getElementById('re_name').textContent = `이름: ${data.name}`;
+        document.getElementById('re_studentNumber').textContent = `학번: $${data.student_id}`;
 
     })
     .catch(error => {
