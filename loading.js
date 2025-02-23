4b25d7afb4690c3f121966b333ff80eec5d7fd98
biyuns <cart0089@naver.com>
2025-02-24 04:12:19 +0900

    test

--- loading.js
+++ loading.js
@@ -14,16 +14,23 @@ setTimeout(() => {
         },
         body : JSON.stringify({name, studentNumber})
     })
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP 오류 발생: ${response.status}`);
+        }
+        return response.json();
+    })
     .then(data => {
-        if(data.drawResult) {
+        console.log("API 응답 데이터:", data);
+        if (data.drawResult && data.drawResult.drawnUser) {
             const drawnUser = data.drawResult.drawnUser;
-            window.location.href =`result_page.html?name=${drawnUser.name}&age=${drawnUser.age}&instagramId=${drawnUser.instagramId}&department=${drawnUser.department}&gender=${drawnUser.gender}&mbti=${drawnUser.mbti}&bio=${drawnUser.bio}`
-
+            window.location.href = `result_page.html?name=${drawnUser.name}&age=${drawnUser.age}&instagramId=${drawnUser.instagramId}&department=${drawnUser.department}&gender=${drawnUser.gender}&mbti=${drawnUser.mbti}&bio=${drawnUser.bio}`;
         } else {
             console.log("오류발생오류발생");
         }
     })
-    .catch(error=> console.error('오류', error));
+    .catch(error => console.error('오류', error));
 
-}, 2000);
+}, 2000)
+
+
