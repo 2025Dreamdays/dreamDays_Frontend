c626edfba861a2c9ee4d5f1af6fdd994bb3cc734
biyuns <cart0089@naver.com>
2025-02-25 15:52:01 +0900

    sdfa

--- result_page.js
+++ result_page.js
@@ -14,7 +14,7 @@ window.onload = function () {
     // 값들을 화면에 표시
     document.getElementById("name").textContent = `${name}`;
     document.getElementById("age").textContent = `${age}`;
-    document.getElementById("instagramId").textContent = `${instagramId}`;
+    document.querySelector("a").textContent = `${instagramId}`;
     document.getElementById("department").textContent = `${department}`;
     document.getElementById("gender").textContent = `${gender}`;
     document.getElementById("mbti").textContent = `${mbti}`;
